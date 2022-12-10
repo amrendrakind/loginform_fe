e1fb900acc6e36811579653522c8793e3a1b89c3
amrendrakind <vision2010@gmail.com>
2022-12-10 22:23:52 +0530

    Update:HomePages

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -3,7 +3,7 @@ import NavBar from '../navbar/NavBar';
 const Homepage = () => (
   <div>
     <NavBar />
-    <h1 className="App">Welcome to Home Page of Innova Labs</h1>
+    <h1 className="App">Welcome to Home Page of My Labs</h1>
   </div>
 );
 export default Homepage;
